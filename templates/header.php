55c225190765a2c73a9c2ccd3c86bcb60c2a5dca
JQD <202678382+JohnQDeveloper@users.noreply.github.com>
2026-05-12 15:04:09 -0400

    switch to beer css

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html lang="<?php echo htmlspecialchars(get_language(), ENT_QUOTES, 'UTF-8'); ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link href="https://cdn.jsdelivr.net/npm/beercss@4.0.21/dist/cdn/beer.min.css" rel="stylesheet">
+    <script type="module" src="https://cdn.jsdelivr.net/npm/beercss@4.0.21/dist/cdn/beer.min.js"></script>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/material-dynamic-colors@1.1.4/dist/cdn/material-dynamic-colors.min.js"></script>
+    <link rel="stylesheet" href="./css/custom.css">
+    <title><?php echo t('index.title'); ?> :: <?php echo ENVIRONMENT; ?></title>
+  </head>
+  <body>
